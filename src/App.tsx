01e38723c9c5c735b6910da7aeda0c7847701a1c
cyd5538 <cyd5538@naver.com>
2022-10-28 00:23:53 +0900

    날씨 모달창까지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="h-screen w-full bg-purple-800 text-white relative">
-      <div className='absolute right-14 top-4'>
+    <div className="h-screen w-full  text-white bg-purple-500 relative">
+      <div className='absolute right-4 top-4'>
         <Weather />
       </div>
     </div>
--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import useGeoLocation from "../../Hooks/useGeoLocation";
 import axios from "axios";
 import { TbTemperatureCelsius } from "react-icons/tb";
@@ -7,11 +7,15 @@ interface weather {
   name: String;
   main: {
     temp: number;
+    feels_like :number;
+    temp_max : number;
+    temp_min : number;
   };
   weather: [
     {
       icon: String;
       description: String;
+      main: String
     }
   ];
 }
@@ -19,6 +23,8 @@ interface weather {
 const Weather = () => {
   const [weatherData, setWeatherData] = useState<weather>();
   const [Icon, setIcon] = useState<string>("");
+  const [modal, setModal] = useState<boolean>(false);
+  const modalclose = useRef<HTMLDivElement>(null)
 
   const location = useGeoLocation();
   const lat: number | undefined = location.coordinates?.lat;
@@ -38,34 +44,106 @@ const Weather = () => {
   };
 
   let iconurl = "http://openweathermap.org/img/w/" + Icon + ".png";
-  let temp = weatherData?.main.temp;
+
 
   useEffect(() => {
     if (lat !== 0) {
       WeatherGet();
     }
   }, [lat, lng]);
+;
+
+
+useEffect(() => {
+    const clickOutside = (e: any) => {
+      if (modal && !modalclose.current?.contains(e.target)) {
+        setModal(false)
+      }
+    }
+    document.addEventListener('mousedown', clickOutside)
+    return () => {
+      document.removeEventListener('mousedown', clickOutside)
+    }
+  }, [modal])
+
+
   return (
-    <div className="flex flex-col">
-      <div className="flex gap-2 items-center">
-        <div>
-          {weatherData ? (
-            <img className="w-12 h-12" src={iconurl} alt="icon"></img>
-          ) : null}
+    <>
+        <div className="flex flex-col cursor-pointer"  onClick={() => setModal(true)}>
+            <div className="flex gap-2 items-center">
+                <div>
+                    {weatherData ? (
+                        <img className="w-10 h-10" src={iconurl} alt="icon"></img>
+                    ) : null}
+                    </div>
+                    <div className="text-3xl font-bold flex items-center gap-1">
+                    {weatherData?.main.temp !== undefined ? (
+                        Math.ceil(weatherData.main.temp)
+                    ) : (
+                        null
+                    )}
+                    <TbTemperatureCelsius />
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <h1 className="text-2xl font-bold">{weatherData?.name}</h1>
+            </div>
         </div>
-        <div className="text-3xl font-bold flex items-center gap-1">
-          {weatherData?.main.temp !== undefined ? (
-            Math.ceil(weatherData.main.temp)
-          ) : (
-            null
-          )}
-          <TbTemperatureCelsius />
+        {/*  dropDown */}
+   
+        {modal ? (
+            <div ref={modalclose} className="bg-white w-80 sm:w-96 h-48 text-black absolute mt-2 drop-shadow-lg border-none rounded-lg p-4 right-0">
+            <h1 className="text-4xl font-bold">{weatherData?.name}</h1>
+            <div className="text-xl text-gray-400">{weatherData?.weather[0].main}</div>
+            <div className="flex justify-around mt-4">
+                <div>
+                    {weatherData ? (
+                        <img className="w-20 h-20" src={iconurl} alt="icon"></img>
+                    ) : null}
+                </div> 
+                <div className="text-4xl pb-2 font-bold flex items-center gap-1">
+                    {weatherData?.main.temp !== undefined ? (
+                        Math.ceil(weatherData.main.temp) 
+                    ) : (
+                        null
+                    )}
+                    <TbTemperatureCelsius />
+                </div>
+                <div>
+                <div>
+                    <div><span className="text-gray-400">Feels like </span>{weatherData?.main.feels_like !== undefined ? (
+                        Math.ceil(weatherData.main.feels_like) 
+                    ) : (
+                        null
+                    )}
+                    <span className="ml-1">º</span>
+                    </div>
+                </div>
+                <div>
+                    <div><span className="text-gray-400">Temp Max</span> {weatherData?.main.temp_max !== undefined ? (
+                        Math.ceil(weatherData.main.temp_max) 
+                    ) : (
+                        null
+                    )}
+                    <span className="ml-1">º</span>
+                    </div>
+                </div>
+                <div>
+                    <div><span className="text-gray-400">Temp Min</span> {weatherData?.main.temp_min !== undefined ? (
+                        Math.ceil(weatherData.main.temp_min)  
+                    ) : (
+                        null
+                    )}
+                    <span className="ml-1">º</span>
+                    </div>
+                </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="flex justify-center">
-        <h1 className="text-3xl font-bold">{weatherData?.name}</h1>
-      </div>
-    </div>
+        ) : (
+          null
+        )}
+    </>
   );
 };
 
